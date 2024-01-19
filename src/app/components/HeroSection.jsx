f1d0f4429b6f340e581756a8ef62932d4831e599
andrewgarrison <atg27@uw.edu>
2024-01-18 21:57:37 -0800

    added header

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 "use client";
-import React from 'react'
-import Image from 'next/image'
-import { TypeAnimation } from 'react-type-animation';
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
@@ -9,38 +9,47 @@ const HeroSection = () => {
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I'm{" "} </span>
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I'm{" "}
+            </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                'Andrew Garrison',
-                1300,
-                'a Web Developer',
-                1300,
-                'a Backend Developer',
-                1300,
-                'Certified in Dev Ops',
-                1300
+                "Andrew Garrison",
+                1000,
+                "a Web Developer",
+                1000,
+                "a Backend Developer",
+                1000,
+                "Certified in Dev Ops",
+                1000,
               ]}
               wrapper="span"
               speed={50}
               repeat={Infinity}
             />
           </h1>
-          <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:te xt-xl'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, adipisci.
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:te xt-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
+            adipisci.
           </p>
           <div>
-            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>Hire me</button>
-            <button className='px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  hover:bg-slate-800 text-white mt-3'><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span></button>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              Hire me
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </button>
           </div>
         </div>
-        <div className='col-span-5 place-self-center mt-4 mb-6 lg:mt-0'>
+        <div className="col-span-5 place-self-center mt-4 mb-6 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] relative">
             <Image
-              src='/images/casspassprofile.png'
+              src="/images/casspassprofile.png"
               alt="hero image"
-              className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
             />
@@ -48,7 +57,7 @@ const HeroSection = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
-import Image from 'next/image'
-import HeroSection from './components/HeroSection'
-import Navbar from './components/Navbar'
+import Image from "next/image";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar/>
-      <div class="container mx-auto px-12 py-4">
-        <HeroSection/>
+      <Navbar />
+      <div class="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
       </div>
     </main>
-  )
+  );
 }
