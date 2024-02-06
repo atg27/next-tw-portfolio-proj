a8b6f55bff46f969069a20e58f4b2c4c0bd3cbd9
andrewgarrison <atg27@uw.edu>
2024-02-06 12:21:45 -0800

    projects section filter and styling

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 const projectsData = [
   {
@@ -38,34 +40,61 @@ const projectsData = [
     gitUrl: "/",
     PreviewUrl: "/",
   },
-  // {
-  //   id: 5,
-  //   title: "Food Ordering App",
-  // description: "Project 5 description",
-  //   image: "/images/projects/1.png",
-  //   tag: ["All", "Web"],
-  // gitUrl: "/",
-  //   PreviewUrl: "/",
-  // },
-  // {
-  //   id: 6,
-  //   title: "React Firebase Template",
-  // description: "Project 6 description",
-  //   image: "/images/projects/1.png",
-  //   tag: ["All", "Web"],
-  // gitUrl: "/",
-  //   PreviewUrl: "/",
-  // },
+  {
+    id: 5,
+    title: "Food Ordering App",
+    description: "Project 5 description",
+    image: "/images/projects/1.png",
+    tag: ["All", "Mobile"],
+    gitUrl: "/",
+    PreviewUrl: "/",
+  },
+  {
+    id: 6,
+    title: "React Firebase Template",
+    description: "Project 6 description",
+    image: "/images/projects/1.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    PreviewUrl: "/",
+  },
 ];
 
 const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) => {
+    return project.tag.includes(tag);
+  });
+
   return (
     <>
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
       <div className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {projectsData.map((project) => (
+        {filteredProjects.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
